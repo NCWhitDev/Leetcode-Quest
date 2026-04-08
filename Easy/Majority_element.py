0c7383576bbce3b110d1952cb029157627e5eb06
N. Connor Whitaker <Connornwhit@gmail.com>
2026-04-07 21:33:17 -0400

    Update Majority_element.py

--- Easy/Majority_element.py
+++ Easy/Majority_element.py
@@ -10,10 +10,22 @@ def main():
   nums = [2,2,1,1,1,2,2] # 2
   print(majorityElement(nums))
 
+# Many ways to do this problem
 
+# 1 way
 def majorityElement(nums):
-  nums_sorted = sorted(nums)
-  return nums_sorted[len(nums) // 2]
+  nums_sorted = sorted(nums) # sort the array
+  return nums_sorted[len(nums) // 2] # return the middle element
+
+def sortbyCount(nums):
+  sorted_count = sorted(count.items(), key=lambda x: x[1], reverse=True)
+  return sorted_count  # [(3, 3), (2, 2), (1, 1)]
+
+def sortbyMax(nums):
+  most_common = max(count, key=lambda x: count[x])
+  print(most_common)   # 3  (the key with the highest count)
+  print(count[most_common])  # 3  (the count itself)
+  return most_common
 
 if __name__ == "__main__":
     main()
